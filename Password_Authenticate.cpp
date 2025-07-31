0e34102fa06a45bb72772a09ce49a6bea4247265
Siddhesh Bhavsar <siddhesh.bhavsar310@gmail.com>
2025-07-31 14:55:01 +0530

    Create Password_Authenticate.cpp

---
+++ Password_Authenticate.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string password, input;
+    password = "Soham"; // Set your password here
+
+    cout << "Enter password: ";
+    cin >> input;
+
+    if(input == password) {
+        cout << "Access granted." << endl;
+    } else {
+        cout << "Access denied. Incorrect password." << endl;
+    }
+    return 0;
+}
+/* output 
+Enter password: passw123
+Access denied. Incorrect password.
+__
+Enter password: Soham
+Access granted. */
